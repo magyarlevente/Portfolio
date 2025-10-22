49d6cc851c035099e82aa26295f8f2bc803dc636
Magyar Levente <magyar.levente25@gmail.com>
2025-10-22 11:16:24 +0200

    About Me section ready

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/Navbar";
+import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
 
 export const Home = () => {
     return (
@@ -15,7 +17,12 @@ export const Home = () => {
         <Navbar />
         {/* Main Content */}
 
+        <main>
 
+            <HeroSection />
+            <AboutSection />
+
+        </main>
         {/* Footer */}
     </div>
     );
